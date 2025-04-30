3500b14635f56d28a505da7288d4699ef9c9076e
franciSauce <careyfrancisowino@gmail.com>
2025-04-30 08:32:07 +0300

    Add css module

--- webpack.config.js
+++ webpack.config.js
@@ -15,4 +15,13 @@ module.exports = {
       template: "./src/template.html",
     }),
   ],
+  module: {
+    rules: [
+      {
+        test: /\.css$/i,
+        use: ["style-loader", "css-loader"],
+      },
+    ],
+  },
 };
+
