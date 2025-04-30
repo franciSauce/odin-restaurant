d65c4e20d36b802e4806479a2c1f211c07c8e517
franciSauce <careyfrancisowino@gmail.com>
2025-04-30 08:19:40 +0300

    Add event listeners to the home, menu & about

--- src/index.js
+++ src/index.js
@@ -1,4 +1,13 @@
-// index.js
-import { greeting } from "./test.js";
+import generateHomePage from "./home";
+import generateMenuPage from "./menu";
+import generateAboutPage from "./about";
 
-console.log(greeting);
+const home = document.querySelector(".home");
+const menu = document.querySelector(".menu");
+const about = document.querySelector(".about");
+
+home.addEventListener("click", generateHomePage);
+menu.addEventListener("click", generateMenuPage);
+about.addEventListener("click", generateAboutPage);
+
+generateHomePage();
